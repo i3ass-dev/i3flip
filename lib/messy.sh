21d3f990edb593f10bd40de77efdbb794e04a5bd
bud <robstenklippa@gmail.com>
2020-07-30 14:55:18 +0200

    finalized new version

--- lib/messy.sh
+++ lib/messy.sh
@@ -1,8 +1,5 @@
 messy() {
-
   # arguments are valid i3-msg arguments
-  # separate resize commands and execute
-  # all commands at once in cleanup()
   (( __o[verbose] )) && ERM "m $*"
   (( __o[dryrun]  )) || _msgstring+="$*;"
 }
