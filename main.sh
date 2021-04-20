63cb4fd951ab1fe585c70ea635eee657bc8742d0
bud <robstenklippa@gmail.com>
2021-04-20 17:08:05 +0200

    [CHANGE] take advantage of new i3viswiz output features

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 main(){
 
@@ -9,31 +9,28 @@ main(){
   ((__o[verbose])) && ERM "target direction: $_dir"
   _dir=${_dir,,}
 
-  eval "$(i3viswiz -p ${__o[json]:+--json "${__o[json]}"} | head -1)"
-  # unset unneeded variabels from viswiz
-  unset trgcon trgx trgy wall trgpar sx sy sw sh groupid
+  read -r layout last first pos size < <(
+    i3viswiz --parent ${__o[json]:+--json "${__o[json]}"} \
+             --debug grouplayout,lastingroup,firstingroup,grouppos,groupsize \
+             --debug-format "%v "
+  )
 
-  # by creating the wiz string, shellcheck will
-  # not complain about un-assigned variables 
-  wiz+="layout:${grouplayout:=} last:${lastingroup:=} "
-  wiz+="first:${firstingroup:=} "
-  wiz+="pos:${grouppos:=} size:${groupsize:=0} "
+  ((__o[verbose])) && ERM "w" "layout:$layout" "last:$last" \
+                              "first:$first" "pos:$pos" "size:$size"
 
-  ((__o[verbose])) && ERM "w $wiz"
-
-  ((groupsize < 2)) && ERX only container in group
+  ((size < 2)) && ERX only container in group
 
   case "${_dir:0:1}" in
 
     r|d|n ) 
       next=1 prev=0
-      [[ "$grouplayout" =~ tabbed|splith ]] \
+      [[ "$layout" =~ tabbed|splith ]] \
         && ldir=right || ldir=down
     ;;
 
     l|u|p )
       prev=1 next=0
-      [[ "$grouplayout" =~ tabbed|splith ]] \
+      [[ "$layout" =~ tabbed|splith ]] \
         && ldir=left || ldir=up
     ;;
 
@@ -41,31 +38,31 @@ main(){
   esac
 
   # focus/move normally
-  if (( (grouppos  > 1 && grouppos < groupsize)       
-   || ( (grouppos == 1 && next)
-   ||   (grouppos == groupsize && prev) ) )); then
+  if (( (pos  > 1 && pos < size)       
+   || ( (pos == 1 && next)
+   ||   (pos == size && prev) ) )); then
    
    ((__o[move])) && cmd=move || cmd=focus
    messy "$cmd  $ldir"
   
   # warp focus/move to end of group
-  elif ((grouppos == 1)); then
+  elif ((pos == 1)); then
     if ((__o[move])); then
-      messy "[con_id=$lastingroup] mark --add --toggle fliptmp"
+      messy "[con_id=$last] mark --add --toggle fliptmp"
       messy "move to mark fliptmp"
-      messy "[con_id=$lastingroup] mark --add --toggle fliptmp"
+      messy "[con_id=$last] mark --add --toggle fliptmp"
     else
-      messy "[con_id=$lastingroup] focus"
+      messy "[con_id=$last] focus"
     fi
 
   # warp focus/move to start of group, (move+swap)
   else
     if ((__o[move])); then
-      messy "[con_id=$firstingroup] mark --add --toggle fliptmp"
+      messy "[con_id=$first] mark --add --toggle fliptmp"
       messy "move to mark fliptmp, swap container with mark fliptmp"
-      messy "[con_id=$firstingroup] mark --add --toggle fliptmp"
+      messy "[con_id=$first] mark --add --toggle fliptmp"
     else
-      messy "[con_id=$firstingroup] focus"
+      messy "[con_id=$first] focus"
     fi
   fi
 
