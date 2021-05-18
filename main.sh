8e1e00399f9301db5d83bf5649d5a75c4ac09aa3
bud <robstenklippa@gmail.com>
2021-05-18 23:31:32 +0200

    .

--- main.sh
+++ main.sh
@@ -2,19 +2,23 @@
 
 main(){
 
+  [[ -f /tmp/i3flip_lock ]] && exit
+  touch /tmp/i3flip_lock
+  trap 'rm /tmp/i3flip_lock' EXIT HUP INT
+
   declare -g _msgstring
   declare -i next prev
 
   _dir=$1
-  ((__o[verbose])) && ERM "target direction: $_dir"
+  ((__o[verbose])) && ERM $'\n'"target direction: $_dir"
   _dir=${_dir,,}
 
   read -r layout last first pos size < <(
     i3viswiz --parent ${__o[json]:+--json "${__o[json]}"} \
              --debug grouplayout,lastingroup,firstingroup,grouppos,groupsize \
              --debug-format "%v "
-  )
-
+    )            
+  
   ((__o[verbose])) && ERM "w" "layout:$layout" "last:$last" \
                               "first:$first" "pos:$pos" "size:$size"
 
@@ -24,13 +28,13 @@ main(){
 
     r|d|n ) 
       next=1 prev=0
-      [[ "$layout" =~ tabbed|splith ]] \
+      [[ $layout =~ tabbed|splith ]] \
         && ldir=right || ldir=down
     ;;
 
     l|u|p )
       prev=1 next=0
-      [[ "$layout" =~ tabbed|splith ]] \
+      [[ $layout =~ tabbed|splith ]] \
         && ldir=left || ldir=up
     ;;
 
